cdea072b806f9593a19919b2462d8a54b2de0742
Nagaraja Jade <hello@build22.org>
2016-04-08 13:29:57 +0100

    Modules, Functions and function clauses

--- my-stuff/length_converter.ex
+++ my-stuff/length_converter.ex
@@ -3,3 +3,23 @@ defmodule MeterToFootConverter do
     m * 3.28084
   end
 end
+
+# defmodule MeterToLengthConverter do
+#   defmodule Inch do
+#     def convert(m) do
+#       m * 39.3701
+#     end
+#   end
+#
+#   defmodule Feet do
+#     def convert(m) do
+#       m * 3.28084
+#     end
+#   end
+# end
+
+defmodule MeterToLengthConverter do
+  def convert(:feet, m), do: m * 3.28
+  def convert(:inch, m), do: m * 39.37
+  def convert(:yard, m), do: m * 1.09
+end
